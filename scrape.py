31ba875791958cc146b3798f9a7a5bc5305c4653
Sudeepa Virajitha <virajithaabekoon@gmail.com>
2023-03-19 15:23:41 +0530

    Added Functions

--- scrape.py
+++ scrape.py
@@ -5,67 +5,101 @@ import time
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 options = Options()
 
-options.headless = False
+# options.add_argument("--headless=new")
 
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
 
-driver = webdriver.Chrome("/media/sudeepa/Common/Python/chromedriver",options=options)
-driver.get("https://www.cbsl.gov.lk/en/rates-and-indicators/exchange-rates/daily-buy-and-sell-exchange-rates")
-# assert "Python" in driver.title
+def CentralBank():
+    driver.get("https://www.cbsl.gov.lk/en/rates-and-indicators/exchange-rates/daily-buy-and-sell-exchange-rates")
+    # assert "Python" in driver.title
+    driver.switch_to.frame("iFrameResizer2")
+    elem = driver.find_element(By.ID, "rangeType_range")
+    elem.click()
+    select = Select(driver.find_element(By.ID, "rangeValue"))
+    select.select_by_index(1)
+    submit = driver.find_element(By.NAME, "submit_button")
+    submit.click()
 
-driver.switch_to.frame("iFrameResizer2")
-elem = driver.find_element(By.ID, "rangeType_range")
-elem.click()
-select = Select(driver.find_element(By.ID, "rangeValue"))
-select.select_by_index(1)
-submit = driver.find_element(By.NAME, "submit_button")
-submit.click()
+    tables = driver.find_elements(By.CLASS_NAME, "table-responsive")
 
-tables = driver.find_elements(By.CLASS_NAME, "table-responsive")
+    for table in tables:
+        soup = BeautifulSoup(table.get_attribute('innerHTML'), "html.parser")
+        usd_table = soup.find_all("table")
 
-for table in tables:
-    soup = BeautifulSoup(table.get_attribute('innerHTML'), "html.parser")
-    usd_table = soup.find_all("table")
+    rates = []
 
-rates = []
+    for usd in usd_table:
+        rows = usd.find_all("td")
+        rates.append(rows[0:3])
 
-for usd in usd_table:
-    rows = usd.find_all("td")
-    rates.append(rows[0:3])
+    print(("{} - {} - {}".format(rates[-1][0].text,rates[-1][1].text,rates[-1][2].text)))
 
-print(("{} - {} - {}".format(rates[-1][0].text,rates[-1][1].text,rates[-1][2].text)))
+def SampathBank():
+    driver.get("https://www.sampath.lk/en/exchange-rates")
 
-driver.get("https://www.sampath.lk/en/exchange-rates")
+    table = driver.find_element(By.CLASS_NAME,"exch-rates")
 
-table = driver.find_element(By.CLASS_NAME,"exch-rates")
+    samp = BeautifulSoup(table.get_attribute("innerHTML"),"html.parser")
 
-samp = BeautifulSoup(table.get_attribute("innerHTML"),"html.parser")
+    rows = samp.find_all("tr")
 
-rows = samp.find_all("tr")
+    dataset = rows[-1].find_all("td")
 
-print(rows[-1])
+    sampathreturn = {
+        "source":"Sampath",
+        "sell_rate":dataset[1].text,
+        "buy_rate":dataset[3].text
+    }
+    print(sampathreturn)
 
-driver.get("https://www.peoplesbank.lk/exchange-rates/")
+def PeoplesBank():
+    driver.get("https://www.peoplesbank.lk/exchange-rates/")
 
-peoples = driver.find_element(By.CLASS_NAME,"table")
+    peoples = driver.find_element(By.CLASS_NAME,"table")
 
-people = BeautifulSoup(peoples.get_attribute("innerHTML"),"html.parser")
+    people = BeautifulSoup(peoples.get_attribute("innerHTML"),"html.parser")
 
-peoplerows = people.find_all("tr")
+    peoplerows = people.find_all("tr")
 
-print(peoplerows[2])
+    print(peoplerows[2])
 
-driver.get("https://www.nsb.lk/rates-tarriffs/exchange-rates/")
+def NSBank():
+    driver.get("https://www.nsb.lk/rates-tarriffs/exchange-rates/")
 
-nsbtable = driver.find_element(By.CLASS_NAME,"table")
+    nsbtable = driver.find_element(By.CLASS_NAME,"table")
 
-nsbsoup = BeautifulSoup(nsbtable.get_attribute("innerHTML"),"html.parser")
+    nsbsoup = BeautifulSoup(nsbtable.get_attribute("innerHTML"),"html.parser")
 
-nsbrows = nsbsoup.find_all("tr")
+    nsbrows = nsbsoup.find_all("tr")
 
-print(nsbrows[2])
+    nsbreturn = {
+        "source":"NSB",
+        "sell_rate":nsbrows[2].find_all("td")[-1].text,
+        "buy_rate":nsbrows[2].find_all("td")[-2].text
+    }
+
+    print(nsbreturn)
+
+    # print(nsbrows[2])
+
+def GoogleFinance():
+    driver.get("https://www.google.com/finance/quote/USD-LKR")
+    rate = driver.find_element(By.CLASS_NAME,"kf1m0")
+    finance = BeautifulSoup(rate.get_attribute("innerHTML"),"html.parser")
+    finance.find("div")
+    googlereturn = {
+        "source":"Google",
+        "sell_rate":finance.text,
+        "buy_rate":finance.text
+    }
+    print(googlereturn)
+
+SampathBank()
 
 time.sleep(10)
 
